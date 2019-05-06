0de8d3f86d2f5afe4c5b35588b7b771ccf0032fc
Darcy Linde <lindedarcy@gmail.com>
2019-05-06 16:18:18 -0400

    updated users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: :ok
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+end
